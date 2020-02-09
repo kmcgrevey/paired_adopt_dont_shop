11ee912b4e742a83d06e688c1ba8bd687406ee5f
Kathleen Carroll <khakicarroll@comcast.net>
2020-02-09 14:44:54 -0700

    Finish and test story 14

---
+++ spec/features/favorites/user_can_see_no_favorites_index_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+RSpec.describe "On favorites index page", type: :feature do
+  it "can see no pets added to favorites if no favorites" do
+    shelter1 = Shelter.create(name: "Pups 4 U",
+                           address: "208 Puppy Place",
+                           city: "Denver",
+                           state: "CO",
+                           zip: 80211)
+
+    pet1 = Pet.create(image_src: "https://thedogsjournal.com/wp-content/uploads/bfi_thumb/bernedoodle-prize-39zr02ajf5v660ii64zbbe.jpg",
+                          name: "Biscuit",
+                          approximate_age: "8",
+                          sex: "F",
+                          shelter_id: shelter1.id)
+
+    visit '/favorites'
+    expect(page).to have_content("No favorited pets")
+    expect(page).to_not have_content(pet1.name)
+
+    visit "/pets/#{pet1.id}"
+    click_on "Add Pet to Favorites"
+
+    visit '/favorites'
+    expect(page).to have_content(pet1.name)
+  end
+end
