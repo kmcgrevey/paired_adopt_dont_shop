861c5e7be22bd26779cf72bd2518ab33d206affc
Kevin McGrevey <niv3kmcg@gmail.com>
2020-02-10 12:18:31 -0700

    Add additional review for stronger testing

--- spec/features/shelters/user_can_delete_shelter_spec.rb
+++ spec/features/shelters/user_can_delete_shelter_spec.rb
@@ -20,6 +20,11 @@ RSpec.describe "Delete Shelter", type: :feature do
                               rating: "5",
                               content: "This place smells SO nice",
                               shelter: @shelter1)
+      review2 = Review.create(title: "Meh",
+                              rating: "3",
+                              content: "I think my mom's basement is better",
+                              image_src: "https://media.npr.org/assets/img/2014/12/31/allen-dogs2_slide-8899d50d4e46c5ad322c5a3a364310f15f555d6f-s800-c85.jpg",
+                                                      shelter: @shelter1)
     end
 
     it "I can delete the shelter" do
