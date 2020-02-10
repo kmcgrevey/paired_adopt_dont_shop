3decef0f6e5c201570cc35918f5ac0cfb1165f23
Kevin McGrevey <niv3kmcg@gmail.com>
2020-02-09 21:58:43 -0700

    Add test for flash message for missing name field

--- spec/features/shelters/user_can_create_new_shelter_spec.rb
+++ spec/features/shelters/user_can_create_new_shelter_spec.rb
@@ -1,29 +1,50 @@
 require 'rails_helper'
 
 RSpec.describe "index to new shelter form page", type: :feature do
-  it "can go to new shelter form and submit" do
-    shelter1 = Shelter.create(name: "Pups 4 U",
-                           address: "208 Puppy Place",
-                           city: "Denver",
-                           state: "CO",
-                           zip: 80211)
+  xit "cannot create new shelter without name" do
+    # shelter1 = Shelter.create(name: "Pups 4 U",
+    #                        address: "208 Puppy Place",
+    #                        city: "Denver",
+    #                        state: "CO",
+    #                        zip: 80211)
 
-    shelter2 = Shelter.create(name: "Furry Friends Shelter",
-                            address: "7899 St Charles Ave",
-                            city: "New Orleans",
-                            state: "LA",
-                            zip: 70118)
+    # shelter2 = Shelter.create(name: "Furry Friends Shelter",
+    #                         address: "7899 St Charles Ave",
+    #                         city: "New Orleans",
+    #                         state: "LA",
+    #                         zip: 70118)
 
     visit '/shelters'
 
     click_on 'Create New Shelter'
+
     expect(current_path).to eq('/shelters/new')
     expect(page).to have_content("Shelter name:")
     expect(page).to have_content("Address:")
     expect(page).to have_content("State:")
     expect(page).to have_content("City:")
     expect(page).to have_content("Zip:")
-
     expect(page).to have_css('input', :count => 6)
+
+    fill_in :city, with: "Denver"
+
+    # save_and_open_page
+    click_on 'Submit'
+
+    expect(page).to have_content("Shelter not created: NAME required.")
+    expect(page).to have_content("New Shelter Information")
+
+    expect(page).to have_button('Submit')
+  end
+
+  it "can go to new shelter form and submit" do
+    visit '/shelters/new'
+
+    fill_in :shelter_name, with: "Pups 4 U"
+
+    click_on 'Submit'
+    visit '/shelters'
+
+    expect(page).to have_content("Pups 4 U")
   end
 end
