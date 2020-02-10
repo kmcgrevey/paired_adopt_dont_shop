193f2435618958d95c30261ee58e9a3267e69b5c
Kevin McGrevey <niv3kmcg@gmail.com>
2020-02-10 12:34:31 -0700

    Add method to delete reviews in order to delete a shelter

--- app/controllers/shelters_controller.rb
+++ app/controllers/shelters_controller.rb
@@ -26,13 +26,6 @@ class SheltersController < ApplicationController
 
   def update
     shelter = Shelter.find(params[:id])
-    # shelter.update({
-    #   name: params[:shelter][:name],
-    #   address: params[:shelter][:address],
-    #   city: params[:shelter][:city],
-    #   state: params[:shelter][:state],
-    #   zip: params[:shelter][:zip]
-    #   })
     shelter.update(shelter_params)
     if shelter.save
       redirect_to "/shelters/#{shelter.id}"
@@ -42,18 +35,29 @@ class SheltersController < ApplicationController
     end
   end
 
-  def destroy #REFACTOR TO MODEL and PORO
-    pet_list = Shelter.find(params[:id]).pets # LIST OF PETS
-    if (pet_list.any? { |pet| (pet.status == "pending") || (pet.status == "approved") }) == false
-      # NEED TO DELETE EACH PET FROM db BEFORE SHELTER
-      pet_list.each do |pet|
-        Pet.destroy(pet.id.to_s)
-      end
+  def destroy #REFACTOR TO MODEL and PORO?
+    @pet_list = Shelter.find(params[:id]).pets
+    if (@pet_list.any? { |pet| (pet.status == "pending") || (pet.status == "approved") }) == false
+      destroy_pets
+      destroy_reviews
       Shelter.destroy(params[:id])
     else
       flash[:alert] = "Meowch! Couldn't delete shelter because of active adoption status."
     end
     redirect_to '/shelters'
+  end
+
+  def destroy_pets
+    @pet_list.each do |pet|
+      Pet.destroy(pet.id.to_s)
+    end
+  end
+
+  def destroy_reviews
+    review_list = Shelter.find(params[:id]).reviews
+    review_list.each do |review|
+      Review.destroy(review.id.to_s)
+    end
   end
 
   private
