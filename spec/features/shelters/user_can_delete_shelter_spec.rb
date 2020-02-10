c224d565e19c3b8fcc93b7f32acdb83508bb1efe
Kevin McGrevey <niv3kmcg@gmail.com>
2020-02-09 17:49:38 -0700

    Add test to not delete shelter with pending pet status

--- spec/features/shelters/user_can_delete_shelter_spec.rb
+++ spec/features/shelters/user_can_delete_shelter_spec.rb
@@ -1,28 +1,62 @@
 require 'rails_helper'
 
-RSpec.describe "index to new shelter form page", type: :feature do
-  it "can go to new shelter form and submit" do
-    shelter1 = Shelter.create(name: "Pups 4 U",
-                           address: "208 Puppy Place",
-                           city: "Denver",
-                           state: "CO",
-                           zip: 80211)
+RSpec.describe "Delete Shelter", type: :feature do
+  describe "when I visit a shelter show page" do
 
-    shelter2 = Shelter.create(name: "Furry Friends Shelter",
-                            address: "7899 St Charles Ave",
-                            city: "New Orleans",
-                            state: "LA",
-                            zip: 70118)
+    before(:each) do
+      @shelter1 = Shelter.create(name: "Pups 4 U",
+        address: "208 Puppy Place",
+        city: "Denver",
+        state: "CO",
+        zip: 80211)
 
-    visit '/shelters'
-    expect(page).to have_content(shelter2.name)
-    expect(page).to have_content(shelter1.name)
+      @shelter2 = Shelter.create(name: "Furry Friends Shelter",
+        address: "7899 St Charles Ave",
+        city: "New Orleans",
+        state: "LA",
+        zip: 70118)
+    end
 
-    visit "/shelters/#{shelter1.id}"
-    click_on 'delete'
+    it "I can delete the shelter" do
+      visit '/shelters'
+      expect(page).to have_content(@shelter2.name)
+      expect(page).to have_content(@shelter1.name)
 
-    visit '/shelters'
-    expect(page).to have_content(shelter2.name)
-    expect(page).to_not have_content(shelter1.name)
+      visit "/shelters/#{@shelter1.id}"
+      click_on 'delete'
+
+      visit '/shelters'
+      expect(page).to have_content(@shelter2.name)
+      expect(page).to_not have_content(@shelter1.name)
+    end
+
+    it "I cannot delete shelter with pending pet status" do
+      pet1 = Pet.create(image_src: "https://free-classifieds-usa.com/oc-content/uploads/893/76682.jpg",
+                            name: "Peanut",
+                            approximate_age: "3",
+                            sex: "M",
+                            shelter_id: @shelter1.id,
+                            status: "pending")
+
+      pet2 = Pet.create(image_src: "https://thedogsjournal.com/wp-content/uploads/bfi_thumb/bernedoodle-prize-39zr02ajf5v660ii64zbbe.jpg",
+                            name: "Biscuit",
+                            approximate_age: "8",
+                            sex: "F",
+                            shelter_id: @shelter1.id,
+                            status: "adoptable")
+
+      visit '/shelters'
+      expect(page).to have_content(@shelter2.name)
+      expect(page).to have_content(@shelter1.name)
+
+      visit "/shelters/#{@shelter1.id}"
+      click_on 'delete'
+
+      expect(page).to have_content("Meowch! Couldn't delete shelter because of pending adoption status.")
+
+      visit '/shelters'
+      expect(page).to have_content(@shelter2.name)
+      expect(page).to have_content(@shelter1.name)
+    end
   end
 end
