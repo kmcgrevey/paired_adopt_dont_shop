df440a441e09263c3d7859c55011acb3d9b50163
Kevin McGrevey <niv3kmcg@gmail.com>
2020-02-11 12:32:09 -0700

    Add average rating method

--- app/models/shelter.rb
+++ app/models/shelter.rb
@@ -3,4 +3,16 @@ class Shelter < ApplicationRecord
   has_many :reviews
 
   validates_presence_of :name
+
+
+    def average_rating
+      # require "pry"; binding.pry
+      if reviews != []
+          # reviews.average(:rating)
+          reviews.average("rating::int")
+      else
+        0
+      end
+    end
+
 end
